9e30c5e277e01e3682e4079e0e5aeada2e746df4
art-nova <artemnovak06@gmail.com>
2023-06-03 06:31:27 +0300

    Replace strong password with long password

--- src/api/users/dtos/create-user.dto.ts
+++ src/api/users/dtos/create-user.dto.ts
@@ -1,12 +1,8 @@
 import { PickType } from '@nestjs/swagger';
-import { IsStrongPassword } from 'class-validator';
+import { MinLength } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends PickType(User, ['nickname', 'email', 'locale', 'timezone']) {
-    /**
-     * Must be a strong password.
-     * @example WnW9Fyst1x
-     */
-    @IsStrongPassword()
+    @MinLength(12)
     password: string;
 }
