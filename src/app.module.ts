f62460574aa5b5562271e2d59a872ec3bfa35374
art-nova <artemnovak06@gmail.com>
2023-06-04 22:42:54 +0300

    Fix Public decorator not working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { classes } from '@automapper/classes';
 import { AutomapperModule } from '@automapper/nestjs';
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './api/auth/auth.module';
 import { UsersModule } from './api/users/users.module';
+import { IsAuthenticatedGuard } from './common/guards/is-authenticated.guard';
+import { AuthConfigService } from './common/processed-config/auth-config.service';
 import { CoreConfigService } from './common/processed-config/core-config.service';
 import { DatabaseConfigService } from './common/processed-config/database-config.service';
 import { HttpConfigService } from './common/processed-config/http-config.service';
 import { OpenApiConfigService } from './common/processed-config/openapi-config.service';
 import { ProcessedConfigModule } from './common/processed-config/processed-config.module';
 import { CleanupModule } from './maintenance/cleanup/cleanup.module';
-import { AuthConfigService } from './common/processed-config/auth-config.service';
 
 @Module({
     imports: [
@@ -37,6 +39,16 @@ import { AuthConfigService } from './common/processed-config/auth-config.service
         UsersModule,
         AuthModule,
     ],
-    providers: [AuthConfigService, CoreConfigService, DatabaseConfigService, HttpConfigService, OpenApiConfigService],
+    providers: [
+        AuthConfigService,
+        CoreConfigService,
+        DatabaseConfigService,
+        HttpConfigService,
+        OpenApiConfigService,
+        {
+            provide: APP_GUARD,
+            useClass: IsAuthenticatedGuard,
+        },
+    ],
 })
 export class AppModule {}
--- src/common/guards/is-authenticated.guard.ts
+++ src/common/guards/is-authenticated.guard.ts
@@ -1,11 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class IsAuthenticatedGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest() as Request;
+    constructor(private readonly reflector: Reflector) {}
 
+    canActivate(context: ExecutionContext): boolean {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (isPublic) {
+            return true;
+        }
+        const request = context.switchToHttp().getRequest() as Request;
         return request.isAuthenticated();
     }
 }
