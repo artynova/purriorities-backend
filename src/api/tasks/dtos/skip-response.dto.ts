24a1e7c81701be9675535e2693ef9809a88204a0
art-nova <artemnovak06@gmail.com>
2023-06-12 20:01:32 +0300

    Remove duplicate dto
    The correct one this time.

--- src/api/tasks/dtos/skip-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PunishmentDto } from '../../users/dtos/punishment.dto';
-import { RewardDto } from '../../users/dtos/reward.dto';
-
-export class SkipResponseDto {
-    reward: RewardDto;
-    punishment: PunishmentDto;
-}
