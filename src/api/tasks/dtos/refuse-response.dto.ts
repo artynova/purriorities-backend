236842180078bbdabcfa2338f9c74d7b4e90de92
art-nova <artemnovak06@gmail.com>
2023-06-12 19:54:30 +0300

    Remove logically duplicate dto

--- src/api/tasks/dtos/refuse-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PunishmentDto } from '../../users/dtos/punishment.dto';
-import { RewardDto } from '../../users/dtos/reward.dto';
-
-export class RefuseResponseDto {
-    reward: RewardDto;
-    punishment: PunishmentDto;
-}
